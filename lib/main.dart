40bb3432388edc63b7ab1d771479141ca315f0b0
kedrakk <keaindrak@gmail.com>
2023-05-12 10:25:56 +0630

    added url config

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:myinfo_v3_sample_webapp/route/routes.dart';
+import 'config/configure_nonweb.dart'
+    if (dart.library.html) 'config/configure_web.dart';
 
 void main() {
+  configureApp();
   runApp(const MyApp());
 }
 
